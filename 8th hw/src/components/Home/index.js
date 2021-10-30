3de08ed4317cd5091281a0bb9fcbfeccb0b7b587
MadBowJam <madbowjam@gmail.com>
2021-10-30 13:21:30 +0300

    done 8th hw

---
+++ 8th hw/src/components/Home/index.js
@@ -0,0 +1,22 @@
+import {Link} from "react-router-dom";
+import {appChangeTextAction} from "../../redux/actions/app.actions";
+import {useDispatch, useSelector} from 'react-redux';
+
+const Home = () => {
+    const dispatch = useDispatch(),
+          {text} = useSelector(state => state.appReducer),
+          changeText = () => {
+            dispatch(appChangeTextAction('Goodbye'));
+    }
+
+    return (
+        <div className='home-container'>
+            <h1>Github Battle: battle your friends and ... stuff</h1>
+            <p>{text}</p>
+            <button className='button' onClick={changeText}>Change</button>
+            <Link to='/battle' className='button'>Battle</Link>
+        </div>
+    )
+}
+
+export default Home;
